d0f9b27794167df95a0a6726cc22d9b850a231fa
juliew12 <wangjulie2506@gmail.com>
2025-10-28 20:14:59 -0400

    added csv dataset file

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ using namespace std;
 int main(){
   ifstream file1("dataset/380K_US_Restaurants_1.csv");
 
-  if (!file1.is_open()) {
-     cout << "Unable to open file" << endl;
-  }
+  // if (!file1.is_open()) {
+  //    cout << "Unable to open file" << endl;
+  // }
 
  for (int i = 0; i < 1; i++) {
      string line;
