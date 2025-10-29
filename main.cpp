541d28977ec6b578eaa7f7431a7d7caac69babd9
juliew12 <wangjulie2506@gmail.com>
2025-10-28 20:10:37 -0400

    added csv dataset file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <queue>
 #include <string>
 #include <unordered_map>
@@ -8,7 +9,25 @@
 using namespace std;
 
 int main(){
+  ifstream file1("dataset/380K_US_Restaurants_1.csv");
 
-  
+  if (!file1.is_open()) {
+     cout << "Unable to open file" << endl;
+  }
+
+ for (int i = 0; i < 1; i++) {
+     string line;
+     getline(file1, line);
+     istringstream stream(line);
+
+     string token;
+     while (getline(stream, token, ',')) {
+         cout << "Column: " << token << endl;
+     }
+
+
+ }
+
+
   return 0;
 }
