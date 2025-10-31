c53c544687f0ed7805bed9dc8da6e04f87a88bba
juliew12 <wangjulie2506@gmail.com>
2025-10-31 00:56:38 -0400

    added code to extract data from dataset in main

--- Hashtable.h
+++ Hashtable.h
@@ -7,10 +7,24 @@ using namespace std;
 class Hashtable {
     private:
     //key: category: City, State
-    //value: a vector of tuples: restaurant name (title), phone, rating, address
-
+    //value: a vector of restaurant objects: restaurant name (title), phone, rating, address
 
     public:
+        void insert(const string& key, Restaurant& obj) {
+
+        }
+
+        void search() {
+
+        }
+
+        int hash(const string& key) {
+
+        }
+
+
+
+
 
 
 };
