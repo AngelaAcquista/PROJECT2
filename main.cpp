3067e0d22ec61aa27dd932ce88e403e36f7bc853
juliew12 <wangjulie2506@gmail.com>
2025-10-31 01:26:11 -0400

    added code to extract data from dataset in main

--- main.cpp
+++ main.cpp
@@ -10,44 +10,53 @@
 using namespace std;
 
 int main(){
-  //testing parsing through one csv file first
-  ifstream file1("dataset/380K_US_Restaurants_1.csv");
 
-  //add an if condition in case the file cannot be opened
-  if (!file1.is_open()) {
-     cerr << "Unable to open file" << endl;
+  //read and extract data from 15 files
+  for (int i = 1; i <= 15; i++) {
+      string filePath = "dataset/380K_US_Restaurants_";
+      filePath += to_string(i) + ".csv";
+      ifstream file(filePath);
+
+      //add error handling if a file cannot be opened
+      if (!file.is_open()) {
+          cerr << "Error opening " << filePath << endl;
+      }
+
+      //to avoid reading the header row
+      string line;
+      getline(file, line);
+
+      //testing the first 10 rows from the dataset for now
+      //for each iteration, extract the necessary data for each restaurant
+      for (int row = 0; row < 10; row++) {
+          string line;
+          getline(file, line);
+          istringstream stream(line);
+          vector<string> restaurantData;
+          string dataPoint;
+
+          //for each row, read the extract the data from the first 7 columns
+          for (int col = 0; col < 7; col++) {
+              if (col == 6) {
+                  getline(stream, dataPoint, '[');
+                  dataPoint = dataPoint.substr(1, dataPoint.size() - 4);
+                  cout << dataPoint << endl;
+                  restaurantData.push_back(dataPoint);
+              }
+              else {
+                  getline(stream, dataPoint, ',');
+                  cout << dataPoint << endl;
+                  restaurantData.push_back(dataPoint);
+              }
+          }
+
+          cout << i << endl;
+
+      }
+
+
+
   }
-
-    //to avoid reading the header row
-    string line;
-    getline(file1, line);
-
-    //testing the first 10 rows from the dataset for now
-    //for each iteration, extract the necessary data for each restaurant
-     for (int i = 0; i < 10; i++) {
-         string line;
-         getline(file1, line);
-         istringstream stream(line);
-         vector<string> restaurantData;
-         string dataPoint;
-
-         //for each row, read the extract the data from the first 7 columns
-         for (int j = 0; j < 7; j++) {
-             if (j == 6) {
-               getline(stream, dataPoint, '[');
-                 dataPoint = dataPoint.substr(0, dataPoint.size() - 3);
-                 cout << dataPoint << endl;
-                 restaurantData.push_back(dataPoint);
-             }
-             else {
-                 getline(stream, dataPoint, ',');
-                 cout << dataPoint << endl;
-                 restaurantData.push_back(dataPoint);
-             }
-         }
-
-    }
-
 
 
 
