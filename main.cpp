94538dc74cbb6f94990944be5973a91f559a0bb7
Angela Acquista <157067144+AngelaAcquista@users.noreply.github.com>
2025-11-01 16:20:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,23 +14,23 @@ int main(){
     vector<vector<string>> restaurantData;
 
   //read and extract data from 15 files
-  for (int i = 1; i <= 15; i++) {
+  for (int i = 1; i <= 15; i++){
+      
       string filePath = "dataset/380K_US_Restaurants_";
       filePath += to_string(i) + ".csv";
       ifstream file(filePath);
 
       //add error handling if a file cannot be opened
-      if (!file.is_open()) {
+      if (!file.is_open()){
+          
           cerr << "Error opening " << filePath << endl;
       }
-
       //to avoid reading the header row
       string line;
       getline(file, line);
-
       //testing the first 10 rows from the dataset for now
       //for each iteration, extract the necessary data for each restaurant
-      for (int row = 0; row < 2; row++) {
+      for (int row = 0; row < 2; row++){
           string line;
           getline(file, line);
           istringstream stream(line);
@@ -38,26 +38,28 @@ int main(){
           string dataPoint;
 
           //for each row, read the extract the data from the first 7 columns
-          for (int col = 0; col < 7; col++) {
-              if (col == 6) {
+          for (int col = 0; col < 7; col++){
+              
+              if (col == 6){
+                  
                   getline(stream, dataPoint, '[');
                   dataPoint = dataPoint.substr(1, dataPoint.size() - 4);
                   currRow.push_back(dataPoint);
               }
-              else {
+              else{
+                  
                   getline(stream, dataPoint, ',');
                   currRow.push_back(dataPoint);
               }
           }
           restaurantData.push_back(currRow);
-
       }
-
   }
-
     //declare a vector of restaurant objects
     vector<Restaurant> restaurants;
-    for (int i = 0; i < 1; i++) {
+    
+    for (int i = 0; i < 1; i++){
+        
        string title = restaurantData[i][0];
        string category = restaurantData[i][2];
        float rating =  stof(restaurantData[i][3]);
@@ -71,8 +73,5 @@ int main(){
         // cout << phone << endl;
         // cout << address << endl;
     }
-
-
-
   return 0;
 }
