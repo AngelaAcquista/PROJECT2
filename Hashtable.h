fc82393974b197bc4ab89e491159f27a3421d821
juliew12 <wangjulie2506@gmail.com>
2025-10-31 23:22:20 -0400

    updated hashtable functions

--- Hashtable.h
+++ Hashtable.h
@@ -1,26 +1,51 @@
 #pragma once
 #include <vector>
-#include "Restaurant.h"
+#include <list>
 #include <string>
+#include "Restaurant.h"
 using namespace std;
 
+//hashtable class structure was gotten from provided textbook: Data Structures and Algorithm Analysis in C++
 class Hashtable {
     private:
-    //key: category: City, State
-    //value: a vector of restaurant objects: restaurant name (title), phone, rating, address
+        //key: [category: City, State]
+        //value: a vector of restaurant objects: <restaurant name (title), phone, rating, address>
+        //each bucket will hash to key-value pairs
+        vector<list<pair<string, vector<Restaurant>>>> table;
+        int numEntries;
+        float loadFactor;
+
+        void rehash() {
+
+        }
+        size_t hash(const string& key) {
+            return 0;
+        }
 
     public:
+        Hashtable() {
+            //initialize the hashtable with default values
+            table.resize(50000);
+            this->numEntries = 0;
+            this->loadFactor =  numEntries/(table.size());
+        }
+
         void insert(const string& key, Restaurant& obj) {
 
         }
 
-        void search() {
-
+        bool search(const string& key) {
+            return false;
         }
 
-        int hash(const string& key) {
-            return 0;
-
+        void makeEmpty() {
+            //deletes all entries in the hashtable
+            for (int i = 0; i < table.size(); i++) {
+                table[i].clear();
+            }
+            table.resize(50000);
+            this->numEntries = 0;
+            this->loadFactor =  numEntries/(table.size());
         }
 
 
