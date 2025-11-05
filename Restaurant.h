baa108a3bf40bbd89ece6340848e189de88d331e
Angela Acquista <157067144+AngelaAcquista@users.noreply.github.com>
2025-11-05 12:47:00 -0500

    Update Restaurant.h

--- Restaurant.h
+++ Restaurant.h
@@ -1,45 +1,36 @@
 #pragma once
 #include <string>
+
 using namespace std;
 
-
-class Restaurant {
+class Restaurant{
     private:
         string title;
         string phone;
         float rating;
         string address;
 
-
     public:
 
         Restaurant() : title(""), phone(""), rating(0.0f), address("") {}
 
-        Restaurant(const string& title, const string& phone, const float& rating, const string& address) {
+        Restaurant(const string& title, const string& phone, const float& rating, const string& address){
+            
             this->title = title;
             this->phone = phone;
             this->rating = rating;
             this->address = address;
         }
 
+        string getTitle()const{ return title; }
 
-        string getTitle() const{
-            return title;
-        }
+        string getPhone()const{ return phone; }
 
-        string getPhone() const{
-            return phone;
-        }
+        float getRating()const{ return rating; }
 
-        float getRating() const{
-            return rating;
-        }
+        string getAddress()const{ return address; }
 
-        string getAddress() const{
-            return address;
-        }
-
-        bool operator>(const Restaurant& other) const{
+        bool operator>(const Restaurant& other)const{
             
             return this->rating > other.rating; 
         }
